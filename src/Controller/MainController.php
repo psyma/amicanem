913b0b99ed191dc9880a06b79162f5aba4fc5572
Rickben <rickbengimeda@5star.com.tw>
2024-09-09 16:48:51 +0800

    sidebar users list updated

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,19 +2,28 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository; 
+use Doctrine\ORM\EntityManagerInterface; 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
+    public function __construct(private UserRepository $userRepository, private EntityManagerInterface $entityManager)
+    {
+    }
+    
     #[Route('/', name: 'app_main')]
     public function index(): Response
     {
+        $users = $this->userRepository->findAll();
         $currentUser = $this->getUser();
 
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            'users' => $users,
             'currentUser' => $currentUser
         ]);
     }
