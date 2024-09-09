9467d607c7199c19a62c8dc3bfeaf3893d4eb178
psyma <rickbengimeda@gmail.com>
2024-09-09 19:59:20 +0800

    service added

---
+++ assets/service/service.js
@@ -0,0 +1,142 @@
+import axios from 'axios'
+
+export default class Service { 
+    createMessage = async (uid, sender, receiver, type, content, timestamp, saveMessages) => {
+        const data = new FormData()
+        data.append("uid", uid)
+        data.append("sender", sender)
+        data.append("receiver", receiver)
+        data.append("type", type)
+        data.append("content", content)
+        data.append("timestamp", timestamp) 
+        data.append("saveMessages", saveMessages)  
+ 
+        try {
+            const response = await fetch("/message", { method: "POST", body: data })
+            return await response
+        } catch(e) {
+            return { ok: false }
+        } 
+    }
+
+    deleteMessage = async (uid, id) => { 
+        try { 
+            const response = await fetch(`/message/${uid}/${id}`, { method: "DELETE" })
+            return await response
+        } catch(e) {
+            return { ok: false }
+        } 
+    }
+
+    createImage = async (uid, sender, file, progressCircle) => {
+        const data = new FormData()
+        data.append('uid', uid)
+        data.append('sender', sender)
+        data.append('file', file)
+
+        try {  
+            const response = await axios.post('/image', data, {
+                onUploadProgress: function (progressEvent) {
+                    const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);  
+                    progressCircle.setAttribute("stroke-dashoffset", `calc(251.2px - (251.2px * ${percentCompleted - 1}) / 100)`);
+                }
+            })  
+            return response
+        } catch(e) {   
+            return { status: 500 }
+        }
+    }
+
+    createAudio = async (uid, sender, file, progressCircle) => {
+        const data = new FormData()
+        data.append('uid', uid)
+        data.append('sender', sender)
+        data.append('file', file)
+
+        try {  
+            const response = await axios.post('/audio', data, {
+                onUploadProgress: function (progressEvent) {
+                    const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);  
+                    progressCircle.setAttribute("stroke-dashoffset", `calc(251.2px - (251.2px * ${percentCompleted - 1}) / 100)`);
+                }
+            })  
+            return response
+        } catch(e) {  
+            return { status: 500 }
+        }
+    } 
+
+    setSeenMessage = async (uid, sender, id) => {
+        const data = new FormData()
+        data.append('uid', uid)
+        data.append('sender', sender)
+        data.append('id', id)
+
+        try {
+            const response = await fetch("/set_seen_message", { method: "POST", body: data })
+            return await response
+        } catch(e) {
+            return { ok: false }
+        }
+    }
+
+    setPairKey = async (uid, sender, publicKey, privateKey) => {
+        const data = new FormData()
+        data.append('uid', uid)
+        data.append('sender', sender)
+        data.append('publicKey', publicKey)
+        data.append('privateKey', privateKey)
+
+        try {
+            const response = await fetch("/set_pairkey", { method: "POST", body: data })
+            return await response
+        } catch(e) {
+            return { ok: false }
+        }
+    }
+
+    getPrivateKey = async (uid, sender) => {
+        try {
+            const response = await fetch(`/get_private_key/${uid}/${sender}`, { method: "GET" }) 
+            return await response
+        } catch(e) {
+            return { ok: false }
+        }
+    }
+
+    getMessages = async (uid, sender, receiver, page, pageSize, toReverse = 0) => {
+        try {
+            const response = await fetch(`/get_messages/${uid}/${sender}/${receiver}/${page}/${pageSize}/${toReverse}`, { method: "GET" }) 
+            return await response
+        } catch(e) {
+            return { ok: false }
+        }
+    }
+
+    getLastMessages = async (uid, id) => {
+        try {
+            const response = await fetch(`/get_last_messages/${uid}/${id}`, { method: "GET" })  
+            return await response
+        } catch(e) {
+            return { ok: false }
+        }
+    } 
+
+    getUsers = async (uid) => {
+        try {     
+            const response = await fetch(`/get_users/${uid}`, { method: "GET" }) 
+            return await response
+        } catch(e) {
+            return { ok: false }
+        }
+    }
+
+    getFirebaseConfig = async (uid) => {
+        try {
+            const response = await fetch(`/get_firebase_config/${uid}`, { method: "GET" }) 
+            return await response
+        } catch(e) {
+            return { ok: false }
+        }
+    } 
+}
