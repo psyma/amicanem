575b97eda0a91e4b46634e8eb6497372faebd8da
Rickben <rickbengimeda@5star.com.tw>
2024-09-11 10:06:30 +0800

    Can select user to chat

--- assets/controllers/main_controller.js
+++ assets/controllers/main_controller.js
@@ -3,9 +3,10 @@ import { Controller } from '@hotwired/stimulus';
 import Utils from '../js/utils';
 import Service from "../service/service"   
 
+import { Drawer } from 'flowbite';
 import CryptoJS from 'crypto-js';
-
 import Pusher from 'pusher-js' 
+import Bowser from 'bowser';
 
 class MessageType {
     static TEXT = 0
@@ -31,14 +32,15 @@ export default class extends Controller {
     static currentUserPublickey = null
     static currentUserPrivatekey = null
     static userToChatId = null
-    static userToChatPublickey = null
+    static userToChatPublickey = null 
 
-    connect = async () => {
+    connect = async () => {  
         this.service = new Service()
 
         Pusher.logToConsole = false;
         this.pusher = new Pusher('7c4d952c51d2be9a8302', { cluster: 'ap1' });
 
+        this.setDarkModeToggle()
         await this.setEncryptionDetails() 
         //const response = await fetch("/message", { method: "POST"  })
         //console.log(response.ok)  
@@ -50,9 +52,14 @@ export default class extends Controller {
             const userElement = document.getElementById(`user${user.id}`) // sidebar user list element
             if (userElement != null) {
                 userElement.onclick = () => {
+                    const name = userElement.getAttribute('name')
                     const publickey = userElement.getAttribute('publickey')
                     this.userTochatPublickey = Utils.base64ToArrayBuffer(publickey)
                     this.userToChatId = user.id
+
+                    this.setUserToChatName(name)
+                    this.setSidebarToggleForMobile() 
+                    this.setMainChatbox()
                 }
             }
 
@@ -62,7 +69,7 @@ export default class extends Controller {
             })
         })
 
-        const sendMessageButton = document.getElementById("sendMessageButton")
+        const sendMessageButton = document.getElementById('sendMessageButton')
         sendMessageButton.onclick = async () => {
             const data = JSON.stringify({
                 sender: this.currentUserValue.id,
@@ -77,7 +84,7 @@ export default class extends Controller {
             const content = [encryptedSenderTextMessage, encryptedReceiverTextMessage].join("<|>") 
             await this.service.createTextMessage(this.uidValue, `user${this.userToChatId}`, this.currentUserValue.id, this.userToChatId, MessageType.TEXT, content, Date.now(), true)
         }
-    }
+    } 
 
     setEncryptionDetails = async () => {   
         if (!this.passphraseValue) {
@@ -99,5 +106,68 @@ export default class extends Controller {
 
     setPusherChannelSubscription = () => {
 
+    }
+
+    setUserToChatName = (name) => {
+        const userToChatName = document.getElementById('userToChatName')
+        userToChatName.textContent = name
+    }
+
+    setDarkModeToggle = () => { 
+        let themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+        let themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+        // Change the icons inside the button based on previous settings
+        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            themeToggleLightIcon.classList.remove('hidden');
+            document.documentElement.classList.add('dark');
+        } else {
+            themeToggleDarkIcon.classList.remove('hidden');
+            document.documentElement.classList.remove('dark');
+        }
+
+        var themeToggleBtn = document.getElementById('theme-toggle');
+
+        themeToggleBtn.addEventListener('click', function() { 
+            // toggle icons inside button
+            themeToggleDarkIcon.classList.toggle('hidden');
+            themeToggleLightIcon.classList.toggle('hidden');
+
+            // if set via local storage previously
+            if (localStorage.getItem('color-theme')) {
+                if (localStorage.getItem('color-theme') === 'light') {
+                    document.documentElement.classList.add('dark');
+                    localStorage.setItem('color-theme', 'dark');
+                } else {
+                    document.documentElement.classList.remove('dark');
+                    localStorage.setItem('color-theme', 'light');
+                }
+
+            // if NOT set via local storage previously
+            } else {
+                if (document.documentElement.classList.contains('dark')) {
+                    document.documentElement.classList.remove('dark');
+                    localStorage.setItem('color-theme', 'light');
+                } else {
+                    document.documentElement.classList.add('dark');
+                    localStorage.setItem('color-theme', 'dark');
+                }
+            } 
+        });  
+    }
+
+    setSidebarToggleForMobile = () => {
+        const browser = Bowser.getParser(window.navigator.userAgent); 
+        if (browser.parsedResult.platform.type == 'mobile') {
+            document.getElementById('separatorSidebarButton').click()
+        }
+    }
+
+    setMainChatbox = () => {
+        const mainChatbox = document.getElementById('mainChatbox')
+        const mainChatboxIntro = document.getElementById('mainChatboxIntro')
+
+        mainChatbox.classList.remove('hidden')
+        mainChatboxIntro.classList.add('hidden')
     }
 }
