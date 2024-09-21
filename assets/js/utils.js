0d9917a85f80795725d198c594acae6cd4d404f3
psyma <rickbengimeda@gmail.com>
2024-09-21 17:52:48 +0800

    Options UI list added

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,5 +1,6 @@
 import Bowser from 'bowser';
 import WaveSurfer from 'wavesurfer.js'
+import { Dropdown } from 'flowbite';
 
 export default class Utils {
     static encryptMessage = async (publicKey, message, chunkSize=190) => {
@@ -756,6 +757,60 @@ export default class Utils {
     }
 
     static createVerticalThreeDotsOptionsElement = () => {
+        function createDropdownElement() {
+            // Create the dropdown container
+            const dropdown = document.createElement('div');
+            dropdown.classList.add('hidden', 'top-0', 'absolute', 'z-[100]', 'w-[12.5rem]', 'mt-2', 'rounded-sm', 'bg-white', 'dark:bg-gray-800', 'shadow-lg', 'border', 'border-gray-100', 'dark:border-gray-600', 'focus:outline-none');
+            dropdown.setAttribute('role', 'menu');
+            dropdown.setAttribute('aria-orientation', 'vertical');
+            dropdown.setAttribute('aria-labelledby', 'menu-button');
+            dropdown.setAttribute('tabindex', '-1'); 
+
+            // List of button items
+            const buttons = [
+                { label: 'Reply', iconPath: 'M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3' },
+                { label: 'Copy', iconPath: 'M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z' },
+                { label: 'Pin', iconPath: 'M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0120.25 6v12A2.25 2.25 0 0118 20.25H6A2.25 2.25 0 013.75 18V6A2.25 2.25 0 016 3.75h1.5m9 0h-9' }, 
+                { label: 'Delete Message', iconPath: 'M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'}, 
+            ];
+
+            // Create buttons
+            buttons.forEach(button => { 
+                const btn = document.createElement('button');
+                btn.classList.add('w-full', 'px-4', 'py-3', 'flex', 'items-center', 'border-b', 'opacity-60', 'dark:opacity-70', 'outline-none', 'text-sm', 'border-gray-200', 'dark:border-gray-600', 'transition-all', 'duration-200', 'text-black',   'hover:bg-gray-50', 'dark:hover:bg-gray-600', 'active:bg-gray-100', 'dark:focus:bg-gray-600');
+
+                if (button.label === 'Delete Message') {
+                    btn.classList.add('text-red-500', 'hover:bg-red-50', 'dark:hover:text-red-50', 'active:bg-red-100', 'dark:hover:bg-red-900'); 
+                }
+                else {
+                    btn.classList.add('dark:text-white')
+                }
+
+                const icon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+                icon.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+                icon.setAttribute('fill', 'none');
+                icon.setAttribute('viewBox', '0 0 24 24');
+                icon.setAttribute('stroke-width', '1.5');
+                icon.setAttribute('stroke', 'currentColor');
+                icon.setAttribute('aria-hidden', 'true');
+                icon.classList.add('h-5', 'w-5', 'mr-3'); 
+               
+
+                const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+                path.setAttribute('stroke-linecap', 'round');
+                path.setAttribute('stroke-linejoin', 'round');
+                path.setAttribute('d', button.iconPath);
+                icon.appendChild(path);
+
+                btn.appendChild(icon);
+                btn.appendChild(document.createTextNode(button.label));
+
+                dropdown.appendChild(btn);
+            });
+
+            return dropdown
+        }
+
         const options = document.createElement('div')
 
         // Create the SVG element
@@ -772,8 +827,16 @@ export default class Utils {
         // Append the path to the SVG
         svg.appendChild(path)
 
+        const dropdownElement = createDropdownElement()
+        const dropdown = new Dropdown(dropdownElement, options, { placement: 'top-end' })
+
         options.classList.add('ml-3', 'mt-2', 'invisible', 'cursor-pointer')
         options.appendChild(svg) 
+        options.appendChild(dropdownElement)
+
+        options.onclick = () => {
+            dropdown.show()
+        }
 
         return options
     }
